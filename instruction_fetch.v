08b61e39c28fbccb83d87336279ef57ec3efdded
Munyaradzi Kaudani <G23525839@shell.seas.gwu.edu>
2025-04-05 14:50:08 -0400

    New instruction fetch

--- instruction_fetch.v
+++ instruction_fetch.v
@@ -3,8 +3,8 @@
 module instruction_fetch (
 							input wire 			clk,
 							input wire 			rst_n,
-							input wire  [31:0] 	branch_target,  
-							input wire 			pc_src, 
+							input wire 			pc_src,
+							input wire 			stall_fetch,
 							output wire [31:0] 	instr,
 							output wire [31:0] 	pc_out
 							);
@@ -16,15 +16,12 @@ module instruction_fetch (
 		$readmemh("mips_program.mem", instr_mem);
 	end
 
-	always @(posedge clk or negedge rst_n)
-		begin
-			if (rst_n == 'b0)
-				pc 	<= 32'h0000;
-			else if (pc_src) 
-				pc 	<= branch_target;
-			else
-				pc 	<= pc + 4;
-		end
+	always @(posedge clk or negedge rst_n) begin
+		if (rst_n == 'b0)
+			pc 	<= 32'h0000;
+		else if (!stall_fetch) 
+			pc 	<= pc + 32'd4;
+	end
 
 	assign instr 	= instr_mem[pc >> 2]; //word aligned access
 	assign pc_out 	= pc;
