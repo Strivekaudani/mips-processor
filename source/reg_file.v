ad23a167630be6e15fb92b29bbbb7172797eeef7
Munyaradzi Kaudani <G23525839@shell.seas.gwu.edu>
2025-04-07 22:46:47 -0400

    ARF Logic

--- source/reg_file.v
+++ source/reg_file.v
@@ -11,4 +11,23 @@ module reg_file (
 	input wire			we,
 	input wire [4:0]    waddr,
 	input wire [31:0]	wdata,
-	);'
+	);
+
+	reg [31:0] reg_array [0:31];
+
+	// Asynchronous read
+	assign rdata1 = reg_array[raddr1];
+	assign rdata2 = reg_array[raddr2];
+
+	// Synchronous write
+	always @(posedge clk or negedge rst_n) begin
+		if (rst_n == 'b0) begin
+			integer i;
+			for (i = 0; i < 32; i = i + 1)
+				reg_array[i] <= 0;
+		end else if (we && waddr != 0) begin
+			// Avoid writing to register $zero
+			reg_array[waddr] <= wdata;
+		end
+	end
+endmodule
