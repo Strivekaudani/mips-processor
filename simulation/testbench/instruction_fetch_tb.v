c7ba3807d6441bbfaebb615833a920ec7aeb8716
Munyaradzi Kaudani <G23525839@shell.seas.gwu.edu>
2025-04-08 02:01:03 -0400

    IF testbench WIP

--- simulation/testbench/instruction_fetch_tb.v
+++ simulation/testbench/instruction_fetch_tb.v
@@ -4,14 +4,24 @@ module instruction_fetch_tb();
 
 	reg 				clk;
 	reg 				rst_n;
-	reg					stall_fetch;
-	wire [31:0] 			instr;
-	wire [31:0] 			pc_out;
+	reg					stall;
+	reg					load_mem_en;
+	reg  [31:0]			load_mem_data;
+	reg	 [3:0]			load_mem_addr;
+	wire [31:0] 		instr;
+	wire [31:0] 		pc_out;
+
+	reg [31:0] instr_mem [0:31];
+
+	integer i;
 
 	instruction_fetch DUT (
 		.clk(clk),
 		.rst_n(rst_n),
-		.stall_fetch(stall_fetch),
+		.stall(stall),
+		.load_mem_en(load_mem_en),
+		.load_mem_data(load_mem_data),
+		.load_mem_addr(load_mem_addr),
 		.instr(instr),
 		.pc_out(pc_out)
 		);
@@ -19,12 +29,27 @@ module instruction_fetch_tb();
 	always #5 clk = ~ clk;
 
 	initial begin
-		clk 		= 0;
-		rst_n 		= 0;
-		stall_fetch = 0;
+		clk				= 0;
+		rst_n 			= 0;
+		stall 			= 1;
+		load_mem_en = 0;
+
 		
 		repeat(10) @(posedge clk);
-		rst_n 		= 1;
+
+		rst_n 	= 1;
+		load_mem_en  = 1;
+
+		for (i = 0; i < 32; i = i + 1) begin
+			instr_mem[i] = $random;
+			load_mem_addr = i;
+			load_mem_data = instr_mem[i]; 
+			repeat(1) @(posedge clk);
+		end
+			
+		stall = 0;
+		load_mem_en = 0;
+		
 
 
 		repeat(100) @(posedge clk);
--- source/instruction_fetch.v
+++ source/instruction_fetch.v
@@ -4,20 +4,21 @@ module instruction_fetch (
 							input wire 			clk,
 							input wire 			rst_n,
 							input wire 			stall,
-							output reg [31:0] 	instr,
-							output reg [31:0] 	pc_out
+							input wire 			load_mem_en,
+							input wire 	[31:0] 	load_mem_data,
+							input wire 	[3:0]	load_mem_addr,
+							output wire [31:0] 	instr,
+							output wire [31:0] 	pc_out
 							);
 
 	reg [31:0] 	pc;
-	reg [31:0] 	instr_mem [0:255];
-
-	initial begin
-		$readmemh("mips_program.mem", instr_mem);
-	end
+	reg [31:0] 	instr_mem [0:31];
 
 	always @(posedge clk or negedge rst_n) begin
 		if (rst_n == 'b0)
 			pc 	<= 32'h0000;
+		else if (load_mem_en) 
+			instr_mem[load_mem_addr] <= load_mem_data;
 		else if (!stall) 
 			pc 	<= pc + 32'd4;
 	end
