f49c6cb32655803347892c7a66e95f4276ae3df3
Munyaradzi Kaudani <G23525839@shell.seas.gwu.edu>
2025-04-07 22:50:37 -0400

    fixed IF logic

--- source/instruction_fetch.v
+++ source/instruction_fetch.v
@@ -3,10 +3,9 @@
 module instruction_fetch (
 							input wire 			clk,
 							input wire 			rst_n,
-							input wire 			pc_src,
-							input wire 			stall_fetch,
-							output wire [31:0] 	instr,
-							output wire [31:0] 	pc_out
+							input wire 			stall,
+							output reg [31:0] 	instr,
+							output reg [31:0] 	pc_out
 							);
 
 	reg [31:0] 	pc;
@@ -19,7 +18,7 @@ module instruction_fetch (
 	always @(posedge clk or negedge rst_n) begin
 		if (rst_n == 'b0)
 			pc 	<= 32'h0000;
-		else if (!stall_fetch) 
+		else if (!stall) 
 			pc 	<= pc + 32'd4;
 	end
 
